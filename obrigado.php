0e0adc74ceb4e9101bf5c7331eb60e7d733f6625
Mauricio3697 <133618458+Mauricio3697@users.noreply.github.com>
2025-03-30 22:28:20 -0300

    Add files via upload

---
+++ obrigado.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Obrigado!</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #000;
+            color: #fff;
+            font-family: "Poppins", sans-serif;
+        }
+        .container {
+            margin-top: 50px;
+        }
+        h1 {
+            color: #ffd700;
+        }
+    </style>
+</head>
+<body>
+    <div class="container text-center">
+        <h1>Orçamento enviado com sucesso!</h1>
+        <p>Obrigado por solicitar o orçamento. Entraremos em contato em breve.</p>
+        <a href="index.php" class="btn btn-primary">Voltar à página inicial</a>
+    </div>
+</body>
+</html>
